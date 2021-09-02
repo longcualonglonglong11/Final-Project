7ee3d7197b386f385a18792f9de2bdf8bdd8c2f8
longcualonglonglong11 <nhlong18@apcs.vn>
2021-09-02 15:44:05 +0700

    init

---
+++ Assets/Project/Scripts/CameraTrack.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityChan{
+    public class CameraTrack : MonoBehaviour
+    {
+        
+        public float smooth = 3.0f;
+        Transform standardPos, frontPos, jumpPos;
+        bool quick = false;
+
+        void Start()
+        {
+            standardPos = GameObject.Find("CamPos").transform;
+            frontPos = GameObject.Find ("FrontPos").transform;
+            jumpPos = GameObject.Find ("JumpPos").transform;
+
+            transform.position = standardPos.position;	
+			transform.forward = standardPos.forward;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Alpha1))
+                setCameraPositionFrontView();
+            else if (Input.GetKey(KeyCode.Alpha2))
+                setCameraPositionJumpView();
+            else setCameraPositionNormalView();
+        }
+
+        void setCameraPositionNormalView ()
+		{
+			if (quick == false) {
+				// the camera to standard position and direction
+				transform.position = Vector3.Lerp (transform.position, standardPos.position, Time.fixedDeltaTime * smooth);	
+				transform.forward = Vector3.Lerp (transform.forward, standardPos.forward, Time.fixedDeltaTime * smooth);
+			} else {
+				// the camera to standard position and direction / Quick Change
+				transform.position = standardPos.position;	
+				transform.forward = standardPos.forward;
+				quick = false;
+			}
+		}
+	
+		void setCameraPositionFrontView ()
+		{
+			// Change Front Camera
+			quick = true;
+			transform.position = frontPos.position;	
+			transform.forward = frontPos.forward;
+		}
+
+		void setCameraPositionJumpView ()
+		{
+			// Change Jump Camera
+			quick = false;
+			transform.position = Vector3.Lerp (transform.position, jumpPos.position, Time.fixedDeltaTime * smooth);	
+			transform.forward = Vector3.Lerp (transform.forward, jumpPos.forward, Time.fixedDeltaTime * smooth);		
+		}
+    }
+}
---
+++ Assets/Project/Scripts/UnitychanControl.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnityChan{
+        [RequireComponent(typeof(Animator))]
+        [RequireComponent(typeof(CapsuleCollider))]
+        [RequireComponent(typeof(Rigidbody))]
+    public class UnitychanControl : MonoBehaviour
+    {
+        float animSpeed = 1.5f;
+        float smoother = 3.0f;
+        
+        float forwardSpeed = 2.5f;
+        float runSpeed = 2.0f;
+        float backwardSpeed = 2.0f;
+        float rotateSpeed = 2.0f;
+        float jumpForce = 4.0f;
+        float h, v;
+
+        bool run;
+        private CapsuleCollider col;
+        private Rigidbody rb;
+        private Vector3 velocity;
+        private float orgColHight;
+		private Vector3 orgVectColCenter;
+		private Animator anim;							
+		private AnimatorStateInfo currentBaseState;		
+
+		private GameObject Ucamera;
+		static int idleState = Animator.StringToHash ("Base Layer.Idle");
+		static int locoState = Animator.StringToHash ("Base Layer.Locomotion");
+		static int jumpState = Animator.StringToHash ("Base Layer.Jump");
+		static int restState = Animator.StringToHash ("Base Layer.Rest");
+
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            col = GetComponent<CapsuleCollider>();
+            rb = GetComponent<Rigidbody>();
+            Ucamera = GameObject.FindWithTag("MainCamera");
+            orgColHight = col.height;
+            orgVectColCenter = col.center;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            h = Input.GetAxis ("Horizontal");				
+			v = Input.GetAxis ("Vertical");			
+            if (v > 0.1 && run)
+                v += 1.0f;	
+			anim.SetFloat ("Speed", v);							
+			anim.SetFloat ("Direction", h); 						
+			anim.speed = animSpeed;								
+			currentBaseState = anim.GetCurrentAnimatorStateInfo (0);
+            if (Input.GetButtonDown("Jump") && !anim.IsInTransition(0)){
+                rb.AddForce (Vector3.up * jumpForce, ForceMode.VelocityChange);
+                anim.SetBool("Jump", true);
+            } else {
+                anim.SetBool("Jump", false);
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                    run = !run;
+            }
+            if (currentBaseState.fullPathHash == jumpState){
+                Ucamera.SendMessage("setCameraPositionJumpView");
+            }
+        }
+
+        void FixedUpdate(){
+            velocity = new Vector3(0, 0, v);
+            velocity = transform.TransformDirection(velocity);
+            if (v > 0.1f){
+                if (run)
+                    velocity *= forwardSpeed + runSpeed;
+                else velocity *= forwardSpeed;
+            }
+            else if (v < -0.1f)
+                velocity *= backwardSpeed;
+
+            transform.localPosition += velocity * Time.fixedDeltaTime;
+            transform.Rotate (0, h * rotateSpeed, 0);
+            /*
+            else if (currentBaseState.fullPathHash == idleState) {
+				if (useCurves) {
+					resetCollider ();
+				}
+				if (Input.GetButtonDown ("Jump")) {
+					anim.SetBool ("Rest", true);
+				}
+			}
+            else if (currentBaseState.fullPathHash == restState) {
+                if (!anim.IsInTransition (0)) {
+                    anim.SetBool ("Rest", false);
+                }
+            }
+            */
+        }
+
+        void resetCollider ()
+		{
+			col.height = orgColHight;
+			col.center = orgVectColCenter;
+		}
+
+    }
+}
